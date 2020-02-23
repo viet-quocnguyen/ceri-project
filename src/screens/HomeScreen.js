aa8173fe2726f80d571ede2359d156695231b799
viet-quocnguyen <viet.nguyen@ceridian.com>
2020-02-22 23:53:48 -0500

    draw google map to the home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,46 +2,73 @@ import React, { Component } from "react";
 import {
   Container,
   Content,
-  Header,
-  Left,
-  Right,
-  Body,
-  Title,
   Button,
-  Icon,
   Footer,
   FooterTab,
-  Text
+  Text,
+  View
 } from "native-base";
+import { StyleSheet } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import mapStyle from "./mapstyle";
 
 class HomeScreen extends Component {
   constructor(props) {
     super(props);
   }
 
-  pressHandler = () => {
-    this.props.navigation.navigate("Profile");
-  };
+  onRegionChange(region) {
+    this.setState({ region });
+  }
 
   render() {
+    var mapStyle = mapStyle;
     return (
-      <Container>
-        <Content>
-          <Button success onPress={this.pressHandler}>
-            <Text>Go to Profile</Text>
-          </Button>
-        </Content>
-
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+          customMapStyle={mapStyle}
+        >
+          <Marker
+            draggable
+            coordinate={{
+              latitude: 37.78825,
+              longitude: -122.4324
+            }}
+            onDragEnd={e => alert(JSON.stringify(e.nativeEvent.coordinate))}
+            title={"Test Marker"}
+            description={"This is a description of the marker"}
+          />
+        </MapView>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "flex-end"
+  },
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
+});
 
 export default HomeScreen;
